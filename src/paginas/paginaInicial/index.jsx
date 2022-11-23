1769e4818311e79c6beac728355a5a367236ad1f
Mizael Felipe <mizaelfelipe@outlook.com>
2022-11-22 21:57:46 -0300

    adicionando menu e estilizando

--- src/paginas/paginaInicial/index.jsx
+++ src/paginas/paginaInicial/index.jsx
@@ -1,7 +1,14 @@
 import Cabecalho from "../../componentes/cabecalho";
+import Menu from "../../componentes/Menu";
 
 export default function PaginaInicial() {
     return (
+        <>
         <Cabecalho/>
+        <Menu/>
+        </>
+        
+
+        
     )
 }
