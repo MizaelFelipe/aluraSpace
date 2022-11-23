3d28ce5b1d5f7169c583bdde35008e285797d5db
Mizael Felipe <mizaelfelipe@outlook.com>
2022-11-22 21:28:26 -0300

    adicionando cabecalho e estilizando, alterando icone da pagina.

--- src/componentes/cabecalho/index.jsx
+++ src/componentes/cabecalho/index.jsx
@@ -1,11 +1,14 @@
 import logo from './logo.png'
 import search from './search.png'
+import styles from './Cabecalho.module.scss'
+
 export default function Cabecalho() {
     return(
-        <header>
+        <header className={styles.cabecalho}>
+
             <img src={logo} alt="Logo alura Space" />
-            <div>
-                <input type="text"
+            <div className={styles.cabecalho__container}>
+                <input className={styles.cabecalho__input} type="text"
                 placeholder="O que voce procura ?"/>
                 <img src={search} alt="icone de lupa" />
             </div>
