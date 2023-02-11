411a4f76bff5a82a076cf8e08311bae34081eb9d
Mizael Felipe <mizaelfelipe@outlook.com>
2023-02-10 21:41:15 -0300

    adicionando a sessao de populares

--- src/componentes/Galeria/Cards/index.jsx
+++ src/componentes/Galeria/Cards/index.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import open from "../../Galeria/open.png"
-import favorito from "../../Galeria/favorito.png"
-
+import React from "react";
+import open from './open.png'
+import favorito from './favorito.png'
 
 export default function Cards({itens, styles}) {
-    return (
-        <ul className={styles.galeria__cards}>
-            {itens.map((itens) => {
-                return (
-                    <li key={itens.id} className={styles.galeria__card}>
-                        <img className={styles.galeria__imagem} src={itens.imagem} alt={itens.título} />
-                        <p className={styles.galeria__descricao}> alt={itens.titulo} </p>
-                        <div>
-                            <p>{itens.creditos}</p>
-                            <span>
-                                <img src={favorito} alt="ícone coração de curtir" />
-                                <img src={open} alt="ícone para abrir modal" />
-                            </span>
-                        </div>
-                    </li>
-                )
-            })}
-        </ul>
-    )
+  return (
+    <ul className={styles.galeria__cards}>
+      {itens.map((item) => {
+        return (
+          <li key={item.id} className={styles.galeria__card}>
+            <img
+              className={styles.galeria__imagem}
+              src={item.imagem}
+              alt={item.titulo}
+            />
+            <p className={styles.galeria__descricao}>{item.titulo}</p>
+            <div>
+              <p>{item.creditos}</p>
+              <span>
+                <img src={favorito} alt="ícone coração de curtir" />
+                <img src={open} alt="ícone de abrir modal" />
+              </span>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
